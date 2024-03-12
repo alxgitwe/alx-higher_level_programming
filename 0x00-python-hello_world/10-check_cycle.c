c322011f0f76e80e2e0cf101d173cf88174cb0f9
alxgitwe <el.najib15@gmail.com>
2024-03-12 15:20:38 -0700

    m

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -11,6 +11,9 @@ int check_cycle(listint_t *list)
 	listint_t *a = list;
 	listint_t *b = list;
 
+	if (list->next == NULL || list == NULL)
+		return (0);
+
 	while (b->next && b)
 	{
 		a = a->next;
