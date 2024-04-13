71cb1dc27a00cf8c83a0ea92fe59fbb66d168220
alxgitwe <el.najib15@gmail.com>
2024-04-12 17:22:02 -0700

    m

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,16 @@
 #!/usr/bin/python3
-"""addition function"""
+"""Module a and b"""
 
 
 def add_integer(a, b=98):
-    """addition a and b"""
-    if ((not isinstance(a, int) and not isinstance(a, float))):
-        raise TypeError("a must be an integer")
-    elif ((not isinstance(b, int) and not isinstance(b, float))):
-        raise TypeError("b must be an integer")
-    return (int(a) + int(b))
+    """b is always 98"""
+
+    if type(a) is not int and type(a) is not float:
+        raise TypeError('a must be an integer')
+    if type(b) is not int and type(b) is not float:
+        raise TypeError('b must be an integer')
+    if type(a) is float:
+        a = int(a)
+    if type(b) is float:
+        b = int(b)
+    return a + b
