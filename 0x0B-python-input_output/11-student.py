0da1011289509f63cf8802bc6624d1aa0d1fe77e
alxgitwe <el.najib15@gmail.com>
2024-04-18 00:40:19 +0000

    m

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -4,10 +4,10 @@
 
 
 class Student:
-    """class"""
+    """student"""
 
     def __init__(self, first_name, last_name, age):
-        """new Student"""
+        """Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
@@ -15,6 +15,11 @@ class Student:
     def to_json(self, attrs=None):
         """representation"""
         if (type(attrs) == list and
-                all(type(y) == str for y in attrs)):
-            return {x: getattr(self, x) for x in attrs if hasattr(self, x)}
+                all(type(x) == str for x in attrs)):
+            return {y: getattr(self, y) for y in attrs if hasattr(self, y)}
         return self.__dict__
+
+    def reload_from_json(self, json):
+        """replace"""
+        for i, j in json.items():
+            setattr(self, i, j)
