b9599a5bc2b3f0cf5478c0bc955ab43905642f76
alxgitwe <el.najib15@gmail.com>
2024-04-25 16:38:02 -0700

    m

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -15,3 +15,24 @@ class Square(Rectangle):
         return '[{}] ({}) {}/{} - {}'.\
                 format(type(self).__name__, self.id, self.x, self.y, self.width)
 
+    @property
+    def size(self):
+        """size"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.height = value
+        self.width = value
+
+    def __update(self, id=None, size=None, x=None, y=None):
+        """update"""
+        if id is not None:
+            self.id = id
+        elif size is not None:
+            self.size = size
+        elif x is not None:
+            self.x = x
+        elif y is not None:
+            self.y = y
+
