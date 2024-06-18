7df9ac9d3c5ef0ca7cdce575c50a3ed61d5b46ea
alxgitwe <el.najib15@gmail.com>
2024-06-18 16:49:29 -0700

    m

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-/* script */
-const numArgv = process.argv.length;
-if (numArgv < 3) {
-  console.log('No argument');
-} else if (numArgv === 3) {
-  console.log('Argument found');
+
+if (process.argv.length === 2) {
+  console.log("No argument");
+} else if (process.argv.length === 3) {
+  console.log("Argument found");
 } else {
-  console.log('Arguments found');
+  console.log("Arguments found");
 }
