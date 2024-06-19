71b5c21c1659784e0f5c8aea8f87fbb349efdbbb
alxgitwe <el.najib15@gmail.com>
2024-06-18 18:12:37 -0700

    m

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const firstArg = process.argv[2];
+const size = Number(firstArg);
+
+if (isNaN(size) || size < 1) {
+  console.log("Missing size");
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log("X".repeat(size));
+  }
+}
