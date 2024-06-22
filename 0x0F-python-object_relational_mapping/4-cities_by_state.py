27c2247f2b2f86cd7636baa7922db28a4846ec43
alxgitwe <el.najib15@gmail.com>
2024-06-22 13:47:49 -0700

    m

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""
+    script
+"""
+from sys import argv
+import MySQLdb
+
+if __name__ == "__main__":
+    conn = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3],
+        charset="utf8"
+    )
+    cur = conn.cursor()
+
+    try:
+        query = """
+        SELECT cities.id, cities.name, states.name AS state_name
+        FROM cities
+        JOIN states ON cities.state_id = states.id
+        ORDER BY cities.id ASC
+        """
+        cur.execute(query)
+        results = cur.fetchall()
+    except MySQLdb.Error:
+        try:
+            results = ("MySQLdb Error")
+        except IndexError:
+            results = ("MySQLdb Error - IndexError")
+
+    for row in results:
+        print(row)
+
+    cur.close()
+    conn.close()
