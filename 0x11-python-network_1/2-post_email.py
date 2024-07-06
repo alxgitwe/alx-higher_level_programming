854635b10393243f8f9729f27678623f853fc507
alxgitwe <el.najib15@gmail.com>
2024-07-05 18:54:00 -0700

    m

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -4,15 +4,12 @@ import urllib.request
 import urllib.parse
 import sys
 
-if len(sys.argv) != 3:
-    print("Usage: ./2-post_email.py <URL> <email>", file=sys.stderr)
-    sys.exit(1)
+if __name__ == "__main__":
+    u = sys.argv[1]
+    v = {"email": sys.argv[2]}
+    d = urllib.parse.urlencode(v).encode("ascii")
 
-url = sys.argv[1]
-email = sys.argv[2]
-
-data = urllib.parse.urlencode({'email': email}).encode('ascii')
-
-with urllib.request.urlopen(url, data) as response:
-    print(response.read().decode('utf-8'))
+    r = urllib.request.Request(u, d)
+    with urllib.request.urlopen(r) as r:
+        print(r.read().decode("utf-8"))
 
