3b4600d0d71db2de323e5aa2c2e385f33e313b9b
alxgitwe <el.najib15@gmail.com>
2024-07-05 18:46:05 -0700

    m

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -3,12 +3,8 @@
 import urllib.request
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: ./1-hbtn_header.py <URL>", file=sys.stderr)
-    sys.exit(1)
 
-url = sys.argv[1]
-
-with urllib.request.urlopen(url) as response:
-    print(response.info().get('X-Request-Id'))
-
+if __name__ == "__main__":
+    with urllib.request.urlopen(sys.argv[1]) as response:
+        k = response.getheader('X-Request-Id')
+        print(k)
