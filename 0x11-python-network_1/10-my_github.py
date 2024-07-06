885c114bb1ac9f222911fc265303491100216515
alxgitwe <el.najib15@gmail.com>
2024-07-05 19:42:03 -0700

    m

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,6 +5,6 @@ import sys
 from requests.auth import HTTPBasicAuth
 
 if __name__ == "__main__":
-    q = HTTPBasicAuth(sys.argv[1], sys.argv[2])
-    res = requests.get("https://api.github.com/user", aut=q)
+    k = HTTPBasicAuth(sys.argv[1], sys.argv[2])
+    res = requests.get("https://api.github.com/user", auth=k)
     print(res.json().get("id"))
