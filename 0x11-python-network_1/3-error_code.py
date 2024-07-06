97d51215dae2460d4a2cae4eec87c7d6fd485d51
alxgitwe <el.najib15@gmail.com>
2024-07-05 19:01:31 -0700

    m

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
+"""sends a request to the URL"""
 import urllib.request
 import urllib.error
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: ./3-error_code.py <URL>", file=sys.stderr)
-    sys.exit(1)
-
-url = sys.argv[1]
-
-try:
-    with urllib.request.urlopen(url) as response:
-        print(response.read().decode('utf-8'))
-except urllib.error.HTTPError as e:
-    print("Error code: {}".format(e.code))
-
+if __name__ == "__main__":
+    r = urllib.request.Request(sys.argv[1])
+    try:
+        urllib.request.urlopen(r)
+        with urllib.request.urlopen(r) as res:
+            b = res.read().decode('utf-8')
+            print(b)
+    except urllib.error.HTTPError as k:
+        print("Error code: {}".format(k.code))
