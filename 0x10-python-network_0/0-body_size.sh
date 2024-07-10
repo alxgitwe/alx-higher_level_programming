63255f95ee60448d7c5fb05237b62d12377eff05
alxgitwe <el.najib15@gmail.com>
2024-07-09 18:35:38 -0700

    m

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-# Check if a URL is provided as an argument
-if [ -z "$1" ]; then
-    echo "Usage: $0 <URL>"
-    exit 1
-fi
+# script
 
-# Send a GET request to the specified URL using curl
-response=$(curl -s -o /dev/null -w "%{size_download}" "$1")
-
-# Display the size of the response body in bytes
-echo "$response"
+curl -s "$1" | wc -c
