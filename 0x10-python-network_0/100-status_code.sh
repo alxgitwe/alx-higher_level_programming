2d0493566680dcc5b6551ced0083b3150cd384b8
alxgitwe <el.najib15@gmail.com>
2024-07-09 19:06:15 -0700

    m

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,12 +1,5 @@
 #!/bin/bash
 
-# Check if the URL is provided as an argument
-if [ -z "\$1" ]; then
-  echo "Usage: \$0 <URL>"
-  exit 1
-fi
+# script
 
-# Use curl to send a request and display only the status code
-status_code=$(curl -s -o /dev/null -w "%{http_code}" "\$1")
-echo "$status_code"
-
+curl -so /dev/null -w "%{http_code}" "$1"
