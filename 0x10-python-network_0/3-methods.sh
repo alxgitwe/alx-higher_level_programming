b82f41de3317b8fc94183eb0e45cb160344556e0
alxgitwe <el.najib15@gmail.com>
2024-07-09 18:46:59 -0700

    m

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Check if a URL was provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <URL>"
-  exit 1
-fi
-
-# Use curl to get the allowed HTTP methods for the given URL
-curl -sI "$1" | grep -i '^Allow:' | sed 's/Allow: //'
+# script
+curl -sI "$1" | grep "Allow" | cut -d " " -f 2-
