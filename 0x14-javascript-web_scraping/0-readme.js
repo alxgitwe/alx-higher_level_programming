410c38817c0cc1a28e6e48501422be8df0882880
alxgitwe <el.najib15@gmail.com>
2024-08-06 16:19:55 -0700

    m

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,23 +1,10 @@
-#!/usr/bin/env node
-
+#!/usr/bin/node
 const fs = require('fs');
 
-// Function to read and display file content
-const displayFileContent = (filePath) => {
-    fs.readFile(filePath, 'utf8', (error, content) => {
-        if (error) {
-            console.error('Error reading file:', error);
-        } else {
-            console.log(content);
-        }
-    });
-};
-
-// Check if a file path argument is provided
-if (process.argv.length < 3) {
-    console.error('Usage: ./0-readme.js <file_path>');
-    process.exit(1);
-}
-
-// Call the function with the provided file path
-displayFileContent(process.argv[2]);
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
