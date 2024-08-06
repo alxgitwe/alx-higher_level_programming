0957f89eff3fcefd40cbf4b310c872a710a1bea5
alxgitwe <el.najib15@gmail.com>
2024-08-06 16:22:49 -0700

    m

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,28 +1,10 @@
-#!/usr/bin/env node
+#!/usr/bin/node
+const ffs = require('fs');
 
-const fs = require('fs').promises;
-
-// Function to read and log file content
-const readFileContent = async (filePath) => {
-    try {
-        const data = await fs.readFile(filePath, 'utf-8');
-        console.log(data);
-    } catch (error) {
-        console.error('Error:', error);
-    }
-};
-
-// Main execution
-const main = () => {
-    const filePath = process.argv[2];
-
-    if (!filePath) {
-        console.error('Usage: ./0-readme.js <file_path>');
-        process.exit(1);
-    }
-
-    readFileContent(filePath);
-};
-
-// Run the main function
-main();
+ffs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
+});
