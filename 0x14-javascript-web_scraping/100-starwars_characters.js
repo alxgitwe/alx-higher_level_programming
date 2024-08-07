cbdc4d735b17fe226642b6b4139e5a64e49de4cd
alxgitwe <el.najib15@gmail.com>
2024-08-06 17:09:54 -0700

    m

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,43 +1,13 @@
 #!/usr/bin/node
-const request = require('request');
-
-// Function to fetch characters from a given movie ID
-const fetchStarWarsCharacters = (movieId) => {
-    const movieUrl = `https://swapi.dev/api/films/${movieId}/`;
-
-    request(movieUrl, (error, response, body) => {
-        if (error) {
-            console.error('Error fetching movie data:', error);
-            return;
-        }
-
-        const movieData = JSON.parse(body);
-        const characterUrls = movieData.characters;
-
-        characterUrls.forEach((characterUrl) => {
-            request(characterUrl, (err, res, charBody) => {
-                if (err) {
-                    console.error('Error fetching character data:', err);
-                } else {
-                    const characterData = JSON.parse(charBody);
-                    console.log(characterData.name);
-                }
-            });
-        });
-    });
-};
-
-// Main execution
-const main = () => {
-    const movieId = process.argv[2];
-
-    if (!movieId) {
-        console.error('Usage: ./100-starwars_characters.js <movie_id>');
-        process.exit(1);
+const rqst = require('request');
+const x = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+rqst.get(x, function (error, response, body) {
+  if (error) console.log(error);
+  else {
+    for (let y = 0; y < JSON.parse(body).characters.length; y++) {
+      rqst.get(JSON.parse(body).characters[y], function (error2, response2, body2) {
+        console.log(JSON.parse(body2).name);
+      });
     }
-
-    fetchStarWarsCharacters(movieId);
-};
-
-// Run the main function
-main();
+  }
+});
